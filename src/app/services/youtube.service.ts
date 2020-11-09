b33ee28694d4ad304f101b500751e5acb3dbd5fd
Alex Lopez <alex2761086444@gmail.com>
2020-11-08 18:46:10 -0600

    Servicio para obtener videos de youtube

--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class YoutubeService {
 
-  constructor() { }
+  private youtubeUrl = 'https://www.googleapis.com/youtube/v3';
+  private apiKey = 'AIzaSyAJ2dLX7JXThgL_CZ-_xZ-P6Xl-7cOx5VU';
+  private playlist = 'UUuaPTYj15JSkETGnEseaFFg';
+  private nextPageToken = '';
+
+  constructor( private http: HttpClient ) {}
+
+  getVideos(){
+
+    const url = `${ this.youtubeUrl }/playlistItems`;
+    const params = new HttpParams()
+    
+    .set('part', 'snippet')
+    .set('maxResults', '10')
+    .set('playlistId', this.playlist)
+    .set('key', this.apiKey)
+    
+    return this.http.get( url, { params } );
+
+  }
+
+
 }
