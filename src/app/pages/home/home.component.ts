b2e4cac09c7abf86e6a938b97518cd0eaf732c33
Alex Lopez <alex2761086444@gmail.com>
2020-11-09 23:40:02 -0600

    Mostrar un modal con el video

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { YoutubeService } from 'src/app/services/youtube.service';
 import { Video } from 'src/app/models/youtube.models';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,7 @@ import { Video } from 'src/app/models/youtube.models';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  
   videos: Video[] = []
 
   constructor( private youtubeService: YoutubeService ) { }
@@ -23,5 +25,26 @@ export class HomeComponent implements OnInit {
 
   }
 
+  mostrarVideo( video: Video ){
+    console.log( video )
+    Swal.fire({
+      html:`
+      <h4> ${video.title} </h4>
+      <hr>
+      <iframe width="100%" 
+      height="315" 
+      src="https://www.youtube.com/embed/ ${ video.resourceId.videoId  }" 
+      frameborder="0" 
+      allow="accelerometer; 
+      autoplay; 
+      clipboard-write; 
+      encrypted-media; 
+      gyroscope; 
+      picture-in-picture" 
+      allowfullscreen>
+      </iframe>
+      ` 
+    })
+  }
 }
  
